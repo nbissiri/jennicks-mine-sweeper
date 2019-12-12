5386b8e6d90d660a17ab3adb7f2ca1c39d670835
Nicolas Bissiri <nickbissiri@gmail.com>
2019-12-11 19:32:28 -0800

    added problem2 to Homework

---
+++ MineSweeper/Homework/problem2.cpp
@@ -0,0 +1,7 @@
+#include "problem2.h"
+#include <iostream>
+// ** add your include statement here ** 
+
+void problem2() {
+    std::cout << "This is problem 2!" << std::endl;
+}
---
+++ MineSweeper/Homework/problem2.h
@@ -0,0 +1,6 @@
+#ifndef PROBLEM2_H
+#define PROBLEM2_H
+
+void problem2();
+
+#endif // PROBLEM2_H
