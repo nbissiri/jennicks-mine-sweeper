4be193bc5b87e41e4c7d132bd657d84a1fb315bd
lufah <jenniferkluu@gmail.com>
2019-12-14 19:15:50 -0800

    Completed problem2

--- MineSweeper/Homework/problem2.cpp
+++ MineSweeper/Homework/problem2.cpp
@@ -1,6 +1,7 @@
 #include "problem2.h"
 #include <iostream>
 #include <string>
+#include <vector>
 // ** Part 1. Add your include statement here! ** 
 
 using namespace std; // This line allows us to use cout, endl, string, and 
@@ -25,13 +26,34 @@ void problem2() {
     cout << endl << "--------------------" << endl;
     
     // ** Part 2. Declare and initialize your vector here! **
+    vector<string> cats {"Lucy", "Click", "Bella", "Sasha"};
     
     // ** Part 3. Use push_back() to add us to the vector! **
+    cats.push_back("Nick");
+    cats.push_back("Jenn");
     
     // ** Part 4. Put your for-each loop here! **
+    for (string catName : cats) {
+        cout << catName << endl;
+    }
     
     // ** Part 5. Put your for loop here! **
+    for (size_t i {0}; i < cats.size(); i++){
+        lengthen(cats.at(i));
+        cout << cats.at(i) << endl;
+    }
     
     // ** Part 6. Put your while loop here! **
-
+    
+    string jenn = "Jenn";
+    lengthen(jenn);
+    string nick = "Nick";
+    lengthen(nick);
+    
+    while(cats.size() > 0) {
+        if (cats.back() != jenn && cats.back() != nick) {
+            cout << "Goodnight, " << cats.back() << " you're the best kitty <3" << endl;
+        }
+        cats.pop_back();
+    }
 }
