e22a04921f151e42e440ccc421c12adc441d106b
Nicolas Bissiri <nickbissiri@gmail.com>
2019-12-27 14:20:02 -0800

    MineField is ready for implementation

--- MineSweeper/JennicksMineSweeper/MineField.cpp
+++ MineSweeper/JennicksMineSweeper/MineField.cpp
@@ -1,10 +1,26 @@
 #include "MineField.h"
 
-MineField::MineField()
+MineField::MineField(size_t width, size_t height, int mines) 
+    : width(width), height(height), mines(mines)
 {
-}
-
-MineField::~MineField()
-{
+    // width, height, and mines are  already
+    // initialized above in the initializer list
+    
+    // Construct a MineField and initialize it with Mines in random positions,
+    // NumberedTiles adjacent to those Mines, and EmptyTiles where there are no
+    // adjacent mines 
+    
+    // see 
+    //      https://en.cppreference.com/w/cpp/numeric/random
+    // for random number libraries and usage
+    
+    // ---
+    
+    
+    
+    
+    
+    
+    // ---
 }
 
--- MineSweeper/JennicksMineSweeper/MineField.h
+++ MineSweeper/JennicksMineSweeper/MineField.h
@@ -1,18 +1,27 @@
 #ifndef MINE_FIELD_H
 #define MINE_FIELD_H
-
+#include <cstddef> // size_t
+#include "Tile.h"
 
 /* MineField defines the grid and field of play for a game of MineSweeper. 
  * Once initialized, each position on the grid will either be a safe spot or a 
  * mine.
  */
 class MineField {
-    
 private:
-
+    size_t width, height; // width and height of the grid
+    int mines;
+    // additional private data members go here
+    
+    
+    
+    
 public:
-    MineField();
-    ~MineField();
+    // constructs MineField
+    MineField(size_t width=10, size_t height=10, int mines=10);
+    
+    // returns the tile at the x, y coordinates
+    Tile &at(size_t x, size_t y);
 };
 
 #endif // MINE_FIELD_H
